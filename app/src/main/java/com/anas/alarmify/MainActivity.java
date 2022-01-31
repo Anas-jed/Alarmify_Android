556878893854a7e07fe0f629988f88ec75638a12
AnasYousuf-PZ <anas.yousuf@parazelsus.pk>
2022-01-31 13:01:53 +0500

    alarm set programmatically with exact time is working, now to improve

--- app/src/main/java/com/anas/alarmify/MainActivity.java
+++ app/src/main/java/com/anas/alarmify/MainActivity.java
@@ -1,26 +1,104 @@
 package com.anas.alarmify;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.anas.alarmify.databinding.ActivityMainBinding;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mainBinding;
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    String tag = "MYOWNTAG";
+    Calendar calendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
 
+        createNotificationChannel();
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        calendar = Calendar.getInstance();
+
+        Intent intent = new Intent(this, AlarmBroadcastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
 
         mainBinding.alarmSetButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
+                setAlarm();
+            }
+        });
 
+        mainBinding.alarmCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelAlarm();
             }
         });
     }
+
+    private void cancelAlarm() {
+        alarmManager.cancel(pendingIntent);
+        Log.d(tag,"Alarm Cancelled");
+    }
+
+    @SuppressLint("NewApi")
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setAlarm() {
+        /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+        }else{
+
+        }*/
+        Log.d(tag,"Current Time: "+ calendar.getTime());
+
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        //calendar.set(Calendar.SECOND, );
+
+        /*alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY,pendingIntent);*/
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+        Log.d(tag,"Alarm Set");
+    }
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "foxandroidReminderChannel";
+            String description = "Channel For Alarm Manager";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("foxandroid",name,importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+
+    }
+
+
 }
